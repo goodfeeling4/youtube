00697e9f5d469a60525764deb6c85495c1d47679
md <mdshahnebaj649@gmail.com>
2025-01-08 21:07:44 +0530

    content hover rounded none and size one pixel up

--- src/component/contentsons/Box.jsx
+++ src/component/contentsons/Box.jsx
@@ -2,8 +2,8 @@
 
 export default function Box() {
   return (
-    <div  className="h-64 dark:text-white w-56 rounded-lg cursor-pointer">
-       <div className='flex overflow-hidden rounded-lg h-40 '>
+    <div  className="h-64 dark:text-white w-56 cursor-pointer ">
+       <div className='flex overflow-hidden rounded-lg hover:rounded-none hover:h-[161px] h-40 '>
       <img alt="" src="https://i.ytimg.com/vi/Rql_q-gcSxo/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDtBVfaNe7wN5LZb3CVjYjrs7cTKQ"/>
       </div>
       <span className=" text-wrap">Allah ka Faislay Aap Ke Khawabon Se Behtar Hain! | Feel Down? Listen| Mufti Menk|</span>
