9ddb070c9e3310fe8c04bbbf93d9883d5aadb3e1
goodfeeling4 <mdshahnebaj649@gmail.com>
2025-02-06 17:50:16 +0000

    image uploading

--- src/absolute/plusclicksons/upload.jsx
+++ src/absolute/plusclicksons/upload.jsx
@@ -1,11 +1,12 @@
 // import React from 'react'
-import upload from '../../assets/absolute/plusclick/upload.svg'
+import upload from "../../assets/absolute/plusclick/upload.svg";
 export default function Upload() {
   return (
-    <div className='flex gap-2 text-nowrap p-1 cursor-pointer'>
-      <img src={upload} alt="" />
-      <span>Upload video</span>
-
-    </div>
-  )
+    <a href="https://goodfeel4ever.4nets.org">
+      <div className="flex gap-2 p-1 rounded-md cursor-pointer text-nowrap hover:bg-slate-200">
+        <img src={upload} alt="" />
+        <span>Upload video</span>
+      </div>
+    </a>
+  );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,95 +1,83 @@
-import Bell from "./headersons/Bell"
+import Bell from "./headersons/Bell";
 // import React from 'react'
-import Three from "./headersons/Three"
-import Logo from "./headersons/Logo"
-import Search from "./headersons/Search"
-import Voice from "./headersons/Voice"
-import Plus from './headersons/Plus'
-import PropTypes from "prop-types"
-// absolute bar 
-import Profile from "../absolute/Profile"
-import Plusclick from "../absolute/plusclick"
-import { useState } from "react"
-
+import Three from "./headersons/Three";
+import Logo from "./headersons/Logo";
+import Search from "./headersons/Search";
+import Voice from "./headersons/Voice";
+import Plus from "./headersons/Plus";
+import PropTypes from "prop-types";
+// absolute bar
+import Profile from "../absolute/Profile";
+import Plusclick from "../absolute/plusclick";
+import { useState } from "react";
 
 export default function Header({ Opensider }) {
-
-  const [plhid, setplhid] = useState(false)
+  const [plhid, setplhid] = useState(false);
   const showplus = () => {
-    setplhid(!plhid)
-  }
+    setplhid(!plhid);
+  };
   const hidplus = () => {
-    setplhid(!plhid)
-  }
+    setplhid(!plhid);
+  };
 
   // profile
-  const [spr, setspr] = useState(false)
+  const [spr, setspr] = useState(false);
   const showprofile = () => {
-    setspr(!spr)
-  }
+    setspr(!spr);
+  };
 
   const hidprofile = () => {
-    setspr(!spr)
-  }
-
+    setspr(!spr);
+  };
 
   return (
     <div>
-      <div className=" flex justify-between my-2 gap-1 items-center">
-        <div className="flex md:gap-8 gap-0 items-center md:ml-6 ml-2 ">
-          <div onClick={Opensider} >
-            <Three ></Three>
+      <div className="flex gap-1 justify-between items-center my-2">
+        <div className="flex gap-0 items-center ml-2 md:gap-8 md:ml-6">
+          <div onClick={Opensider}>
+            <Three></Three>
           </div>
-          <div className=" w-16">
+          <div className="w-16">
             <Logo />
           </div>
         </div>
-        <div className=" flex sm:gap-5 gap-2 items-center ">
+        <div className="flex gap-2 items-center sm:gap-5">
           <div className="w-[40vw] justify-end flex ">
             <Search></Search>
           </div>
           <Voice />
         </div>
-        <div className="flex gap-2 items-center ">
+        <div className="flex gap-2 items-center">
           <div className="relative">
-            <div>
+            <a href="https://imgupl.vercel.app/">
               <Plus showplus={showplus} hidplus={hidplus}></Plus>
-
-            </div>
-            <div style={{ display: plhid ? 'block' : 'none' }}>
+            </a>
+            <div style={{ display: plhid ? "block" : "none" }}>
               <Plusclick></Plusclick>
-
             </div>
           </div>
-          <div >
+          <div>
             <Bell></Bell>
           </div>
           <div className="relative">
             <button
-              onClick={showprofile} onBlur={hidprofile}
-              className="text-white sm:text-lg text-xs flex items-center sm:px-2 sm:py-1 px-2 py-1 mr-[2vw] hover:bg-red-500 bg-red-600 rounded-full cursor-pointer">
-              <p >M</p>
-            </button>
-            <div
-              style={{ display: spr ? 'block' : 'none' }}
+              onClick={showprofile}
+              onBlur={hidprofile}
+              className="text-white sm:text-lg text-xs flex items-center sm:px-2 sm:py-1 px-2 py-1 mr-[2vw] hover:bg-red-500 bg-red-600 rounded-full cursor-pointer"
             >
+              <p>M</p>
+            </button>
+            <div style={{ display: spr ? "block" : "none" }}>
               <Profile></Profile>
-
             </div>
           </div>
         </div>
-
       </div>
       {/* {console.log(this.props.)} */}
     </div>
-  )
-
-
-
+  );
 }
-
-
 
 Header.propTypes = {
   Opensider: PropTypes.any,
-}
+};
